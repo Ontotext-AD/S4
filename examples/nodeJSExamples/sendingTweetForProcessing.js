a26153deed6afa5f0ea74281153a4a4f81345c40
Georgi Naumov <gonaumov@gmail.com>
2014-10-06 17:21:13 +0300

    Add text for a license on the places, where it is omitted.

--- examples/nodeJSExamples/sendingTweetForProcessing.js
+++ examples/nodeJSExamples/sendingTweetForProcessing.js
@@ -1,3 +1,20 @@
+/*
+ S4 Web Page Annotation Browser Plugin
+ Copyright  2013, 2014, Ontotext AD
+
+ This file is free software; you can redistribute it and/or modify it under
+ the terms of the GNU Lesser General Public License as published by the Free
+ Software Foundation; either version 2.1 of the License, or (at your option)
+ any later version.
+ This library is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
+ You should have received a copy of the GNU Lesser General Public License along
+ with this library; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
 var https = require('https');
 
 var inputString = ['{"text":"Nearly 200,000 people have been killed in #Syria since the start of the conflict in 2011, according to the U.N. http://t.co/pK7t8AD7Xf","lang":"en",',
