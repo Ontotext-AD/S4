d075fc400342e1f96667327bf7242e2f82b875bc
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2015-03-16 14:47:39 +0200

    Changed uimaFIT sdk to Apache License and removed unwanted files.

--- addons/uimafit-s4-sdk/src/main/scripts/example.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# vim: set sw=4 sts=4 et foldmethod=indent :
-
-source s4pipeline -t NEWS \
-                    -k s4nfnim4rnmt \
-                    -s ukrb34e2feea31l \
-                    -i ~/s4_stuff/input \
-                    -o ~/s4_stuff/output
-
