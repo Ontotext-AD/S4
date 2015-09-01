c21a5b01f8404d0a5d4f765b30e5928cb667297b
sslavov93 <sslavov93@gmail.com>
2015-09-01 10:51:57 +0300

    Added image processing example

---
+++ examples/RubyExamples/processImages.rb
@@ -0,0 +1,63 @@
+# Copyright  2013, 2014, Ontotext AD
+#
+# This file is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+# You should have received a copy of the GNU Lesser General Public License along
+# with this library; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+# IMPORTANT!!! Please note the packages below, needed for this example IMPORTANT!!!
+require "typhoeus"
+require "json"
+require "zlib"
+require "stringio"
+
+
+endpoint = "https://text.s4.ontotext.com/v1"
+service = "/twitie"
+key = "<api-key>"
+secret = "<api-secret>"
+img_tag = true
+img_cat = true
+
+headers = {'Accept' => "application/json",
+                'Content-Type'=> "application/json"}
+data = {
+    "document" => "text goes here",
+    "documentType" => "text/html",
+    "imageTagging" => img_tag,
+    "imageCategorization" => img_cat
+}
+jsonData = data.to_json
+
+hydra = Typhoeus::Hydra.hydra
+req = Typhoeus::Request.new(endpoint+service,
+    method: :post,
+    userpwd: key + ":" + secret, 
+    body: jsonData,
+    headers: headers)
+hydra.queue(req)
+hydra.run
+response = req.response
+
+if response.headers['Content-Encoding'] == 'gzip'
+    gz = Zlib::GzipReader.new(StringIO.new(response.body.to_s))    
+    puts gz.read, "\n"
+else
+    puts response.body, "\n"
+end
+
+# Response Code
+print 'Status Code: ', response.code, "\n\n"
+
+# Response Headers
+puts 'Headers: '
+response.headers.each do |type, header|
+    print type, ': ', header, "\n"
+end
