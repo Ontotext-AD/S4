1ded7f084d6f47ef921b834871bd4e343f1d8e7f
sslavov93 <sslavov93@gmail.com>
2016-04-22 18:17:09 +0300

    Fixed crash when coming accross an unknown Annotation type

--- addons/uima-standard-s4-annotator/src/com/ontotext/s4/api/restclient/S4Endpoints.java
+++ addons/uima-standard-s4-annotator/src/com/ontotext/s4/api/restclient/S4Endpoints.java
@@ -1,12 +1,11 @@
 /*
- * Copyright (c) 2014
+ * Copyright 2016 Ontotext AD
  *
- * This file is part of the s4.ontotext.com REST client library, and is
- * licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
--- addons/uima-standard-s4-annotator/src/com/ontotext/s4/api/types/twitie/Location.java
+++ addons/uima-standard-s4-annotator/src/com/ontotext/s4/api/types/twitie/Location.java
@@ -1,19 +1,18 @@
-/* Self-Service Semantic Suite
-Copyright (c) 2014, Ontotext AD, All rights reserved.
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 3.0 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library.
-*/
+/*
+ * Copyright 2016 Ontotext AD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /* First created by JCasGen Tue Mar 10 18:00:03 EET 2015 */
 package com.ontotext.s4.api.types.twitie;
