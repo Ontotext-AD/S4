f44facf227da47c75ab0aba6d6737b54e08c6fbd
sslavov93 <sslavov93@gmail.com>
2016-05-13 15:52:03 +0300

    Updated JavaScript examples

--- examples/JavaScriptExamples/processTweet.js
+++ examples/JavaScriptExamples/processTweet.js
@@ -21,7 +21,18 @@ limitations under the License.
         var url = "https://text.s4.ontotext.com/v1/twitie";
         var method = "POST";
 
-        var inputString = "<tweet text here>";
+        var inputString = "{\"text\":\"Nearly 200,000 people have been killed in #Syria " +
+            "since the start of the conflict in 2011, according to " +
+            "the U.N. http://t.co/pK7t8AD7Xf\"," +
+            "\"lang\":\"en\",\"entities\":{\"symbols\":[]," +
+            "\"urls\":[{\"expanded_url\":\"http://on.wsj.com/1pZmkY9\"," +
+            "\"indices\":[112,134],\"display_url\":\"on.wsj.com/1pZmkY9\"," +
+            "\"url\":\"http://t.co/pK7t8AD7Xf\"}]," +
+            "\"hashtags\":[{\"text\":\"Syria\",\"indices\":[42,48]}]," +
+            "\"user_mentions\":[]}," +
+            "\"id\":502743846716207104," +
+            "\"created_at\":\"Fri Aug 22 09:07:28   000 2014\"," +
+            "\"id_str\":\"502743846716207104\"}";
 
         var postData = {
             "document": inputString,
